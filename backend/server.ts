a10292c2888dc5df1c0de5193c5041c3b89493d0
gilbertmunuotz <officialgilbert24@gmail.com>
2025-03-19 22:23:01 +0300

    cleaned up app for initial workload

--- backend/server.ts
+++ backend/server.ts
@@ -1,23 +1,34 @@
-import express, { Request, Response, NextFunction } from 'express';
+// **** npm Packages **** //
+import express from "express";
+import dotenv from "dotenv";
+import HttpStatusCodes from "./constants/HttpStatusCodes";
+import { Request, Response, NextFunction } from "express";
 
+// Load env variables
+dotenv.config();
 
-/******************************************************************************
-                                Setup
-*******************************************************************************/
-
+// **** Initiate express app **** //
 const app = express();
-
-
-// **** Middleware **** //
 
 // Basic middleware
 app.use(express.json());
 
+// **** Test Route **** //
+app.get("/api", (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.send("Welcome Cognihub");
+    } catch (error) {
+        console.error("Error Getting Signal", error);
+        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({ status: "error", message: "Internal Server Error" });
+        next(error);
+    }
+});
 
+// **** Start & Listen to Server **** //
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server Listening on Port ${port}...🚀`);
+});
 
-
-/******************************************************************************
-                                Export default
-******************************************************************************/
-
+// Export for testing
 export default app;
