4015fa668716afa8a2956c416bb151a734456aed
gilbertmunuotz <officialgilbert24@gmail.com>
2025-04-04 12:17:25 +0300

    defined logout logic & added up  backend endpoint URI

--- frontend/constants/constant.ts
+++ frontend/constants/constant.ts
@@ -1,1 +1,2 @@
-export const SERVER_URI = "http://localhost:8000"
+export const SERVER_URI = "http://localhost:8000";
+export const CLIENT_URI = "http://localhost:3000";
--- frontend/interfaces/interface.ts
+++ frontend/interfaces/interface.ts
@@ -2,11 +2,4 @@ interface ChatFormProps {
     onChatInteraction: (response: string) => void;
 }
 
-interface GoogleUser {
-    googleId: string;
-    displayName: string;
-    email?: string; // Email is optional because it might not always be available
-    avatar?: string; // Avatar URL is also optional
-}
-
-export type { ChatFormProps, GoogleUser };
+export type { ChatFormProps };
