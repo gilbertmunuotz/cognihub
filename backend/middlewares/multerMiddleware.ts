7592e71e88fad4969e735ec50129dc9504f20121
gilbertmunuotz <officialgilbert24@gmail.com>
2025-04-15 20:04:51 +0300

    updates on multer config middleware

--- backend/middlewares/multerMiddleware.ts
+++ backend/middlewares/multerMiddleware.ts
@@ -1,14 +1,26 @@
+import path from "path";
 import multer from "multer";
-import { Request, NextFunction } from "express";
+import { fileURLToPath } from "url";
+import { Request } from "express";
+
+// Get __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the upload directory (backend/public/)
+const uploadDir = path.join(__dirname, "../public");
 
 // Configure Multer for storing uploads in "/public" directory
 const storage = multer.diskStorage({
     destination: (req: Request, file, cb) => {
-        cb(null, "/public") // Save files in "public" folder)
+        cb(null, uploadDir) // Save files in "public" folder
     },
     filename: (req: Request, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`); // Unique filename
     }
 })
 
-export const upload = multer({ storage });
+export const upload = multer({
+    storage,
+    limits: { fileSize: 10 * 1024 * 1024 } // Limit file size to 1MB
+});
