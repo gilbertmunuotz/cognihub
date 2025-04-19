e4a96a56562131dbb9467d54e16de38f05b71ab6
gilbertmunuotz <officialgilbert24@gmail.com>
2025-04-19 17:34:57 +0300

    updated service controller

--- backend/controllers/service.ts
+++ backend/controllers/service.ts
@@ -1,6 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { SERVER_URI } from "../constants/constant";
 import { NextFunction, Request, Response } from "express";
 import HttpStatusCodes from "../constants/HttpStatusCodes";
-import { SERVER_URI } from "../constants/constant";
+import { extractTextFromPDF } from "../utilities/pdfReader";
+import { extractTextFromDOCX } from "../utilities/docxReader";
+
+
+// Get __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 export async function HandleFiles(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
@@ -9,13 +19,29 @@ export async function HandleFiles(req: Request, res: Response, next: NextFunctio
             return;
         }
 
+        // Get file details
+        const filePath = path.join(__dirname, "../public", req.file.filename);
+        const fileExt = path.extname(req.file.originalname).toLowerCase();
+
+        let extractedText = '';
+
+        if (fileExt === ".pdf") {
+            extractedText = await extractTextFromPDF(filePath);
+        } else if (fileExt === ".docx") {
+            extractedText = await extractTextFromDOCX(filePath);
+        } else {
+            res.status(HttpStatusCodes.BAD_REQUEST).json({ message: "Unsupported file type." });
+            return;
+        }
+
         // File info
         res.json({
             message: "File uploaded successfully!",
             fileUrl: `${SERVER_URI}/public/${req.file?.filename}`,
+            text: extractedText
         });
     } catch (error) {
-        console.error("Registration error:", error);
-        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Registration failed" });
+        console.error("File upload error:", error);
+        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({ message: "File upload failed" });
     }
 };
